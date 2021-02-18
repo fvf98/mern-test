d6e6fc4d948ec30435bb61558c50aab41b0ed756
fvf98 <fvf98@outlook.com>
2021-02-17 22:07:21 -0700

    React-redux default | front

--- memories/src/App.js
+++ memories/src/App.js
@@ -1,5 +1,8 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { Container, AppBar, Typography, Grow, Grid } from '@material-ui/core';
+import { useDispatch } from 'react-redux';
+
+import { getPosts } from './actions/posts';
 
 import Form from './components/Form/Form';
 import Posts from './components/Posts/Posts';
@@ -8,6 +11,11 @@ import useStyles from './styles';
 
 const App = () => {
   const classes = useStyles();
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(getPosts());
+  }, [dispatch]);
 
   return (
     <Container maxWidth='lg'>
