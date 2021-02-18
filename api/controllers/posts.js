b96d7e0d7b7b845b38d5d4e38184137daf37085a
fvf98 <fvf98@outlook.com>
2021-02-18 16:46:44 -0700

    Update post

--- api/controllers/posts.js
+++ api/controllers/posts.js
@@ -1,3 +1,5 @@
+import mongoose from 'mongoose';
+
 import PostMessage from '../models/postMessage.js';
 
 export const getPosts = async (req, res) => {
@@ -19,4 +21,17 @@ export const createPost = async (req, res) => {
     } catch (error) {
         res.status(404).json({ message: error.message })
     }
+};
+
+export const updatePost = async (req, res) => {
+    const { id } = req.params;
+    const { title, message, creator, selectedFile, tags } = req.body;
+
+    if (!mongoose.Types.ObjectId.isValid(id)) return res.status(404).send(`No post with id: ${id}`);
+
+    const updatedPost = { creator, title, message, tags, selectedFile, _id: id };
+
+    await PostMessage.findByIdAndUpdate(id, updatedPost, { new: true });
+
+    res.json(updatedPost);
 };
--- memories/src/reducers/posts.js
+++ memories/src/reducers/posts.js
@@ -1,13 +1,12 @@
-export default (state = [], action) => {
+export default (posts = [], action) => {
     switch (action.type) {
         case 'FETCH_ALL':
-
             return action.payload;
         case 'CREATE':
-
-            return [...state, action.payload];
-
+            return [...posts, action.payload];
+        case 'UPDATE':
+            return posts.map((post) => (post._id === action.payload._id ? action.payload : post));
         default:
-            return state;
+            return posts;
     }
 }
